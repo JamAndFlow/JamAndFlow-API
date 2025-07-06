a87523abe6b68bed7b4ec15a357ebca31727aef9
KevinRawal <kevinrawal30@gmail.com>
2025-07-06 18:52:13 +0530

    added user model and schema

---
+++ app/models/user.py
@@ -0,0 +1,29 @@
+from enum import Enum
+
+from sqlalchemy import Boolean, Column, DateTime
+from sqlalchemy import Enum as SQLEnum
+from sqlalchemy import Integer, String
+
+from app.config.database import Base
+
+
+class AuthType(Enum):
+    LOCAL = "local"
+    GOOGLE = "google"
+    GITHUB = "github"
+    # Add more as needed
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True, index=True, nullable=False)
+    name = Column(String, nullable=False)
+    password = Column(String, nullable=True)  # Only for local auth
+    is_active = Column(Boolean, default=True)
+    provider_id = Column(
+        String, unique=True, nullable=True
+    )  # For OAuth or external providers
+    auth_type = Column(SQLEnum(AuthType), nullable=False, default=AuthType.LOCAL)
+    created_at = Column(DateTime, nullable=False)
---
+++ app/schemas/users.py
@@ -0,0 +1,48 @@
+from datetime import datetime
+from enum import Enum
+from typing import Optional
+
+from pydantic import BaseModel, EmailStr
+
+
+class AuthType(str, Enum):
+    local = "local"
+    google = "google"
+    github = "github"
+    # Add more as needed
+
+
+class UserBase(BaseModel):
+    email: EmailStr
+    full_name: str
+    is_active: bool = True
+    auth_type: AuthType = AuthType.local
+    provider_id: Optional[str] = None
+
+
+class UserCreate(UserBase):
+    password: Optional[str] = None  # Required for local, ignored for OAuth
+
+
+class UserUpdate(BaseModel):
+    email: Optional[EmailStr] = None
+    full_name: Optional[str] = None
+    password: Optional[str] = None
+    is_active: Optional[bool] = True
+    auth_type: Optional[AuthType] = None
+    provider_id: Optional[str] = None
+
+
+class UserInDB(UserBase):
+    id: int
+    created_at: datetime
+
+    class Config:
+        orm_mode = True
+
+
+class UserResponse(UserBase):
+    id: int
+
+    class Config:
+        orm_mode = True
